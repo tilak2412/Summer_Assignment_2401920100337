8f89df206194faba4007fbe553e70d6bc9c60b01
tilak2412 <tilakraj2455@gmail.com>
2026-06-15 20:58:48 +0530

    Day_3_Assignment

---
+++ Week_3/Day_1/LinkedListCyclee.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;          // move 1 step
+            fast = fast.next.next;     // move 2 steps
+
+            if (slow == fast) {
+                return true;           // cycle found
+            }
+        }
+
+        return false;                  // reached end, no cycle
+    }
+}
