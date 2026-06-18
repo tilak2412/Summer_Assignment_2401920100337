af3e01877b89e7a97601935244b310b5fd48d748
tilak2412 <tilakraj2455@gmail.com>
2026-06-18 21:12:13 +0530

    Day_4_Assignment

---
+++ Week_3/Day_4/largestRectInHisto.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() &&
+                   currHeight < heights[stack.peek()]) {
+
+                int height = heights[stack.pop()];
+
+                int rightBoundary = i;
+                int leftBoundary = stack.isEmpty() ? -1 : stack.peek();
+
+                int width = rightBoundary - leftBoundary - 1;
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
