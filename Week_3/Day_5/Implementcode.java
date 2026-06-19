57a73fe4443b74e63bd1299a5a01962c0cad8c9c
tilak2412 <tilakraj2455@gmail.com>
2026-06-19 13:39:52 +0530

    Day_5_Assignment

---
+++ Week_3/Day_5/Implementcode.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        peek(); // Ensure outStack has elements
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
